970434b14d80e40daff387708d8e984c8b380a70
Michał Bachta <michal.bachta@outlook.com>
2018-01-31 09:22:06 +0100

    Fullscreen fixes & AlphabethActivyty as main Activity

--- ProjectMcQueen/app/src/main/java/com/example/micha/projectmcqueen/activities/AlphabethActivity.java
+++ ProjectMcQueen/app/src/main/java/com/example/micha/projectmcqueen/activities/AlphabethActivity.java
@@ -1,10 +1,13 @@
 package com.example.micha.projectmcqueen.activities;
 
+import android.annotation.SuppressLint;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
@@ -29,6 +32,7 @@ import com.google.android.exoplayer2.util.Util;
 public class AlphabethActivity extends AppCompatActivity implements AlphabethAdapter.AlphabethAdapterOnClickHandler {
     private static final String LETTER_INDEX_KEY = "letter-index";
     private static final String ITEM_INDEX_KEY = "item-index";
+    private static final int UI_ANIMATION_DELAY = 300;
 
     private ActivityAlphabethBinding binding;
     private AlphabethAdapter adapter;
@@ -36,13 +40,27 @@ public class AlphabethActivity extends AppCompatActivity implements AlphabethAda
 
     private SimpleExoPlayer exoPlayer;
 
+    private final Handler hideHandler = new Handler();
+    private final Runnable hideRunnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            binding.llMain.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_alphabeth);
         binding.alphabethTopPart.tvLetters.setTypeface(Typeface.createFromAsset(getAssets(), "GloriaHallelujah.ttf"));
@@ -83,6 +101,21 @@ public class AlphabethActivity extends AppCompatActivity implements AlphabethAda
     protected void onStop() {
         super.onStop();
         releasePlayer();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeActivityFullscreen();
+    }
+
+    private void makeActivityFullscreen() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        hideHandler.removeCallbacks(hideRunnable);
+        hideHandler.postDelayed(hideRunnable, UI_ANIMATION_DELAY);
     }
 
     //region Clicks
