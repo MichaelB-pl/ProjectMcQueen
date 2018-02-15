43821d9cf2bb4296cca47fb10c1b6f37515701ee
Michał Bachta <michal.bachta@outlook.com>
2018-02-15 15:07:59 +0100

    Refactor

--- ProjectMcQueen/app/src/main/java/com/example/micha/projectmcqueen/activities/AlphabethActivity.java
+++ ProjectMcQueen/app/src/main/java/com/example/micha/projectmcqueen/activities/AlphabethActivity.java
@@ -3,42 +3,16 @@ package com.example.micha.projectmcqueen.activities;
 import android.annotation.SuppressLint;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
-import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 
 import com.example.micha.projectmcqueen.R;
-import com.example.micha.projectmcqueen.adapters.AlphabethAdapter;
 import com.example.micha.projectmcqueen.databinding.ActivityAlphabethBinding;
 import com.example.micha.projectmcqueen.fragments.AlphabethMainFragment;
-import com.example.micha.projectmcqueen.models.AlphabethItem;
 import com.example.micha.projectmcqueen.viewmodels.AlphabethViewModel;
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.Format;
-import com.google.android.exoplayer2.LoadControl;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.TrackGroup;
-import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class AlphabethActivity extends AppCompatActivity {
     private static final int UI_ANIMATION_DELAY = 300;
@@ -64,9 +38,6 @@ public class AlphabethActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-
-//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_alphabeth);
         viewModel = ViewModelProviders.of(this).get(AlphabethViewModel.class);
