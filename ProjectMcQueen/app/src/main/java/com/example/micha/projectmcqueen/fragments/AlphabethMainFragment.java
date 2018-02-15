4deb3419c050a278c392c8876d3726dddbdf6b89
Michał Bachta <michal.bachta@outlook.com>
2018-02-15 11:53:05 +0100

    it

---
+++ ProjectMcQueen/app/src/main/java/com/example/micha/projectmcqueen/fragments/AlphabethMainFragment.java
@@ -0,0 +1,31 @@
+package com.example.micha.projectmcqueen.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.micha.projectmcqueen.viewmodels.AlphabethViewModel;
+
+/**
+ * Created by micha on 15.02.2018.
+ */
+
+public class AlphabethMainFragment extends Fragment {
+
+    AlphabethViewModel viewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ ProjectMcQueen/app/src/main/java/com/example/micha/projectmcqueen/fragments/AlphabethSpellFragment.java
@@ -0,0 +1,8 @@
+package com.example.micha.projectmcqueen.fragments;
+
+/**
+ * Created by micha on 15.02.2018.
+ */
+
+public class AlphabethSpellFragment {
+}
